ddd1f90c3ea760e8fd1300ef35982b2481e71a40
Alexandru Alexa <x.alx@yahoo.com>
2021-02-28 08:55:35 -0800

    build project

---
+++ src/main/java/com/aa/ClientApi1Application.java
@@ -0,0 +1,13 @@
+package com.aa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClientApi1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClientApi1Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/aa/ClientApi1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClientApi1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
