1a70e914f24150738c36d8b1b9b1099e1fcb01c4
Alexandru Alexa <x.alx@yahoo.com>
2021-03-01 11:14:50 -0800

    RestTemplate authentication 2

--- src/main/java/com/aa/ClientApi1Application.java
+++ src/main/java/com/aa/ClientApi1Application.java
@@ -2,6 +2,8 @@ package com.aa;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 public class ClientApi1Application {
@@ -9,6 +11,10 @@ public class ClientApi1Application {
 	public static void main(String[] args) {
 		SpringApplication.run(ClientApi1Application.class, args);
 	}
-
 	
+	@Bean
+	public RestTemplate restTemplate() {
+		return new RestTemplate();
+	}
+
 }
--- src/main/java/com/aa/service/CustomerServiceImpl.java
+++ src/main/java/com/aa/service/CustomerServiceImpl.java
@@ -1,11 +1,14 @@
 package com.aa.service;
 
+import java.nio.charset.Charset;
 import java.util.List;
 
+import org.apache.tomcat.util.codec.binary.Base64;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
@@ -15,25 +18,37 @@ import com.aa.model.Customer;
 
 @Service
 public class CustomerServiceImpl implements CustomerService {
-	
-	private RestTemplate restTemplate;
-	
-	String url;
-	
+
 	@Autowired
-	public CustomerServiceImpl(@Value("${api-1}") String theUrl, RestTemplateBuilder RTB ) {
-		this.restTemplate = RTB.basicAuthentication("alexandru", "0000").build();
-		this.url = theUrl;
+	private RestTemplate restTemplate;
+
+	@Value("${api-1}")
+	String url;
+
+	public HttpHeaders createHeaders(String username, String password) {
+		return new HttpHeaders() {
+			/**
+			 * 
+			 */
+			private static final long serialVersionUID = 1L;
+
+			{
+				String auth = username + ":" + password;
+				byte[] encodedAuth = Base64.encodeBase64(auth.getBytes(Charset.forName("US-ASCII")));
+				String authHeader = "Basic " + new String(encodedAuth);
+				set("Authorization", authHeader);
+			}
+		};
 	}
 
 	@Override
 	public List<Customer> getCustomers() {
-		
+
 		ResponseEntity<List<Customer>> responseEntity = restTemplate.exchange(
-																				url,
+																				url, 
 																				HttpMethod.GET,
-																				null,
-																				new ParameterizedTypeReference<List<Customer>> () {}
+																				new HttpEntity<>(createHeaders("aa", "0000")),
+																				new ParameterizedTypeReference<List<Customer>>() {}
 																			  );
 		
 		List<Customer> customers = responseEntity.getBody();
